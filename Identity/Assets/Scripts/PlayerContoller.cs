75366886ddd7adc502f096d5d7f6682d9914e200
Yashwa122 <yyousaf2021@elam.co.uk>
2022-06-29 10:43:40 +0100

    i

---
+++ Identity/Assets/Scripts/PlayerContoller.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skull_code : MonoBehaviour
+{
+    //public gameobject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //fixed update()
+    //{
+    //  vector2(player.moveposition)
+    //}
+}
