21445db7cd17f2cb0674501a1e41be4dbb24e439
Yashwa122 <yyousaf2021@elam.co.uk>
2022-07-07 12:45:28 +0100

    move enemies

--- Identity/Assets/Scripts/EnemyMovement.cs
+++ Identity/Assets/Scripts/EnemyMovement.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    
+    public Transform theEnemies;   
+    public Transform startPoint;
+    public Transform endPoint;
+
+    public float speed;
+
+    bool goingBack = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        theEnemies.position = startPoint.position;
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if (goingBack == false)
+        {
+            theEnemies.position = Vector3.MoveTowards(theEnemies.position, endPoint.position, speed);
+
+            if (theEnemies.position == endPoint.position)
+            {
+                goingBack = true;
+            }
+
+        }
+        else
+        {
+            theEnemies.position = Vector3.MoveTowards(theEnemies.position, startPoint.position, speed);
+
+            if (theEnemies.position == startPoint.position)
+            {
+                goingBack = false;
+            }
+        }
     }
 }
